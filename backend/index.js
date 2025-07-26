555582550337786c7f77b9916f3302133dd6f316
SHAIK WASEEM AHMED <waseem@WASEEMs-MacBook-Air.local>
2025-07-26 22:50:02 +0530

    Added new resources for DevOps.

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,10 @@ app.get("/", (req, res) => {
 app.get("/api/videos", (req, res) => {
     res.json([
         { id: 1, title: "Intro to DevOps", url: "https://youtu.be/dQw4w9WgXcQ" },
-        { id: 2, title: "CI/CD Explained", url: "https://youtu.be/scEDHsr3APg?si=kO9C3mMMRfYjW7NK" }
+        { id: 2, title: "CI/CD Explained", url: "https://youtu.be/scEDHsr3APg?si=kO9C3mMMRfYjW7NK" },
+        { id: 2, title: "DevSecOps Explained", url: "https://www.youtube.com/watch?v=atWfA2TW0K0" },
+        { id: 2, title: "Mastering DevOps", url: "https://www.youtube.com/watch?v=PfjVXb-YXbQ" },
+        
     ]);
 });
 
