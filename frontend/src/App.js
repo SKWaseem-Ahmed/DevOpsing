dc6439fd9028657995baf08746346224fd202d88
SHAIK WASEEM AHMED <waseem@WASEEMs-MacBook-Air.local>
2025-07-28 21:36:46 +0530

    Updated the backend call from frontend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const [hiddenCards, setHiddenCards] = useState({});
 
   useEffect(() => {
-    fetch("http://localhost:5050/api/videos")
+    fetch("http://backend-service:5050/api/videos")
       .then((res) => res.json())
       .then((data) => setVideos(data))
       .catch((err) => console.error("Failed to fetch videos:", err));
